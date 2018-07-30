1e66bcfb6b183f2b5283edae8a8fc974e3b6e244
Ivan Smollet <smollet777@bigmir.net>
2018-07-30 16:00:42 +0300

    subscriptions

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,45 @@
 import { Observable } from 'rxjs';
 
-let observable = Observable.create((observer: any) => {
-  observer.next('Hello world!');
-  observer.next('Hello again!');
-  observer.complete();
-  observer.next('Will not be received')
+let observable1 = Observable.create((observer: any) => {
+  observer.next('Observable One is alive!');
+  setInterval(() => {
+    observer.next('Observable One')
+  }, 5000)
 });
 
-observable.subscribe(
-  (result: any) => logItem(result),
-  (error: any) => logItem(`Error: ${error}`),
-  () => logItem('Completed')
+let observable2 = Observable.create((observer: any) => {
+  observer.next('Observable Two is alive!');
+  setInterval(() => {
+    observer.next('Observable Two')
+  }, 2500)
+});
+
+let subscription1 = observable1.subscribe(
+  (result: any) => logItem(result, 1),
 )
 
-function logItem(val: any) {
+let subscription2 = observable2.subscribe(
+  (result: any) => logItem(result, 2),
+)
+
+document.getElementById('unsunscribeBtn1').addEventListener('click', () => {
+  subscription1.unsubscribe()
+})
+document.getElementById('unsunscribeBtn2').addEventListener('click', () => {
+  subscription2.unsubscribe()
+})
+
+document.getElementById('addSubscriptionBtn').addEventListener('click', () => {
+  subscription2.add(subscription1)
+})
+
+function logItem(val: any, column: any) {
   let node = document.createElement('li');
   let textNode = document.createTextNode(val);
   node.appendChild(textNode);
-  document.getElementById('list').appendChild(node);
+  if (column == 1) {
+    document.getElementById('list1').appendChild(node);
+  } else if (column == 2) {
+    document.getElementById('list2').appendChild(node);
+  }
 }
