624b1f2c82635648c7f2913a2b39fe7f13145a23
Ivan Smollet <smollet777@bigmir.net>
2018-08-17 18:05:09 +0300

    cold observable

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,20 @@
 import { Observable } from 'rxjs';
 
 let observable1 = Observable.create((observer: any) => {
-  observer.next('Observable One is alive!');
-  setInterval(() => {
-    observer.next('Observable One')
-  }, 5000)
+  observer.next(`Observable One is alive: ${Date.now()}`);
 });
 
-let observable2 = Observable.create((observer: any) => {
-  observer.next('Observable Two is alive!');
-  setInterval(() => {
-    observer.next('Observable Two')
-  }, 2500)
+document.getElementById('sunscribeBtn1').addEventListener('click', () => {
+  let subscription1 = observable1.subscribe(
+    (result: any) => logItem(result, 1),
+  );
 });
 
-let subscription1 = observable1.subscribe(
-  (result: any) => logItem(result, 1),
-)
-
-let subscription2 = observable2.subscribe(
-  (result: any) => logItem(result, 2),
-)
-
-document.getElementById('unsunscribeBtn1').addEventListener('click', () => {
-  subscription1.unsubscribe()
-})
-document.getElementById('unsunscribeBtn2').addEventListener('click', () => {
-  subscription2.unsubscribe()
-})
-
-document.getElementById('addSubscriptionBtn').addEventListener('click', () => {
-  subscription2.add(subscription1)
-})
+document.getElementById('sunscribeBtn2').addEventListener('click', () => {
+  let subscription2 = observable1.subscribe(
+    (result: any) => logItem(result, 2),
+  );
+});
 
 function logItem(val: any, column: any) {
   let node = document.createElement('li');
